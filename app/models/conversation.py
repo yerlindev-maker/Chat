187ce93b165549c1caca040a5118cbb5ae749654
yerlin <yerlindev@gmail.com>
2026-02-07 14:45:38 -0500

    update conversation models and service

--- app/models/conversation.py
+++ app/models/conversation.py
@@ -2,8 +2,13 @@ from dataclasses import dataclass, field
 from typing import List
 from .message import Message
 
-@dataclass
+
 class Conversation:
+    #def __init__(self, conversation_id, user_id):
+     #   self.conversation_id = conversation_id
+      #  self.user_id = user_id
+        
+    
     conversation_id: str
     user_id: str
     messages: List[Message] = field(default_factory=list)
--- app/services/conversation_service.py
+++ app/services/conversation_service.py
@@ -1,60 +1,22 @@
-from typing import Dict
 from datetime import datetime
+from typing import Dict
 from uuid import uuid4
 
-from app.models.conversation import Conversation, Message
+from app.models.conversation import Conversation
+from app.models.message import Message
+
 
 class ConversationService:
     def __init__(self):
-        #Diccionario que almacena todas las conversaciones en memoria
-        #La clave es conversation_id
-        #El valor es un objeto Conversation
         self._conversations = {}
-
-    def get_or_create(self, conversation_id):
-        """
-        Devuelve una conversación existente si el ID ya existe.
-        Si no existe, crea una nueva conversación.
-        """
-        #Caso 1: El cliente envía conversation_id
-        if conversation_id is not None:
-            #Si ya existe, la devolvemos
-            if conversation_id in self._conversations:
-                return self._conversations[conversation_id]
-            
-            #Si no existe, creamos una nueva con ese mismo ID
-            new_id = conversation_id
-        else:
-            #Caso 2: El cliente NO envía ID -> generamos uno nuevo
-            new_id = str(uuid4())
-
-        #crear una conversación vacía
-        conversation = Conversation(
-            id=new_id,
-            messages=[]
-        )
-
-        #Guardarla en memoria
-        self._conversations[new_id] = conversation
-
-        return conversation
-
-    def add_message(self, conversation, role, content):
-        """
-        Agrega un mensaje a una conversación existente
-        """
-
-        message = Message(
-            role=role,
-            content=content,
-            timestamp=datetime.utcnow()
-        )
-
-        conversation.messages.append(message)
-
-    def get_history(self, conversation):
-        """
-        Devuelve el historial completo de mensajes de una conversación.
-        """
-        return conversation.messages
     
+    def create(self, user_id):
+        #Generar el id
+        new_id = str(uuid4())
+
+        #Crear objeto Conversation
+        conversation = Conversation(
+        conversation_id=new_id,
+        user_id=user_id
+        )
+        
