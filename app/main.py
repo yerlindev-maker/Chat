4edcd031dbdc07405bdaec13d7453c1587bda14b
yerlin <yerlindev@gmail.com>
2026-02-11 21:34:15 -0500

    create tables in main

--- app/main.py
+++ app/main.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI
 from app.routers import health, chat
 
+from app.db.database import Base, engine
+from app.models import conversation, message
+
+Base.metadata.create_all(bind=engine)
+
 app = FastAPI(
     title="Chat API",
     version="1.0.0",
