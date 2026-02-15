29b03e873058f73d1b698baa1835503b33274c70
yerlin <yerlindev@gmail.com>
2026-02-14 19:58:39 -0500

    app/models/message.py

--- app/models/message.py
+++ app/models/message.py
@@ -1,17 +1,28 @@
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy import String, ForeignKey, DateTime
 from datetime import datetime
+
 from app.db.database import Base
 
 class Message(Base):
     __tablename__ = "messages"
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    conversation_id: Mapped[str] = mapped_column(
-        ForeignKey("conversation.id")
-    )
-    role: Mapped[str] = mapped_column(String)
-    content: Mapped[str] = mapped_column(String)
-    timestamp: Mapped[datetime] = mapped_column(DateTime, default=datetime.utcnow)
 
-    conversation = relationship("Conversation", back_populates="messages")
+    conversation_id: Mapped[str] = mapped_column(
+        ForeignKey("conversations.id"),
+        nullable=False
+    )
+
+    role: Mapped[str] = mapped_column(String, nullable=False)
+    content: Mapped[str] = mapped_column(String, nullable=False)
+
+    timestamp = Mapped[datetime] = mapped_column(
+        DateTime,
+        default=datetime.utcnow
+    )
+
+    conversation = relationship(
+        "Conversation",
+        back_populates="messages"
+    )
