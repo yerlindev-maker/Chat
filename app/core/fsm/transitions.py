73af52ffc8162b7ea10155b7f247c42606ec0c3f
yerlin <yerlindev@gmail.com>
2026-04-09 15:13:21 -0500

    modified transitions

--- app/core/fsm/transitions.py
+++ app/core/fsm/transitions.py
@@ -3,7 +3,6 @@
 from dataclasses import dataclass
 from typing import Callable, Optional
 from app.core.fsm.states import StateName
-from states import STATES
 from datetime import datetime
 
 @dataclass
@@ -21,9 +20,14 @@ def start_chat (user_input: str):
     return True
 
 #MENU
-def option_select(option: str):
-    options = ["1", "2", "3"]
-    return option in options    
+def option_1(user_input: str):
+    return user_input == "1"
+
+def option_2(user_input: str):
+    return user_input == "2"
+
+def option_3(user_input: str):
+    return user_input == "3"
 
 #COLLECTING_NAME
 def validate_name(user_input: str):
@@ -62,8 +66,79 @@ def validate_date (user_input: str):
 #CONFIRMING
 #Confirmación del usuario
 def user_confirms(user_input: str):
-    return user_input.lower in ["si", "sí", "yes" "confirmo"]
+    return user_input.lower() in ["si", "sí", "yes" "confirmo"]
 
 #Cancelación del usuario
 def user_cancels(user_input: str):
     return user_input.lower() in ["no", "cancelar", "nop"]
+
+#TRANSITIONS
+TRANSITIONS=[    
+    Transition(
+        from_state=StateName.IDLE,
+        condition=start_chat,
+        to_state=StateName.GREETING
+    ),
+    Transition(
+        from_state=StateName.GREETING,
+        to_state=StateName.MENU
+    ),
+    Transition(
+        from_state=StateName.MENU,
+        condition=option_1,
+        to_state=StateName.COLLECTING_NAME
+    ),
+    Transition(
+        from_state=StateName.MENU,
+        condition=option_2,
+        to_state=StateName.COLLECTING_ID
+    ),
+    Transition(
+        from_state=StateName.MENU,
+        condition=option_3,
+        to_state=StateName.COLLECTING_ID
+    ),
+    Transition(
+        from_state=StateName.COLLECTING_NAME,
+        condition=validate_name,
+        error_message= ,
+        to_state=StateName.COLLECTING_ID
+    ),
+    Transition(
+        from_state=StateName.COLLECTING_ID,
+        condition=validate_id,
+        to_state=StateName.COLLECTING_PLACE
+    ),
+    Transition(
+        from_state=StateName.COLLECTING_PLACE,
+        condition=validate_place,
+        to_state=StateName.COLLECTING_DATE
+    ),
+    Transition(
+        from_state=StateName.COLLECTING_DATE,
+        condition=validate_date,
+        to_state=StateName.CONFIRMING
+    ),
+    Transition(
+        from_state=StateName.CONFIRMING,
+        condition=user_confirms,
+        to_state=StateName.CONFIRMED
+    ),
+    Transition(
+        from_state=StateName.CONFIRMING,
+        condition=user_cancels,
+        to_state=StateName.CANCELLED
+    ),
+    Transition(
+        from_state=StateName.CONFIRMED,
+        to_state=StateName.MENU
+    ),
+    Transition(
+        from_state=StateName.CANCELLED,
+        to_state=StateName.MENU
+    ),
+    Transition(
+        from_state=StateName.ERROR,
+        to_state=StateName.MENU
+    )
+]
